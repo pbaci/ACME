05affc5185bca218a45b91004b64641089b57df6
pbaci <113857593+pbaci@users.noreply.github.com>
2024-07-15 05:24:15 +0000

    fixed Left- to RightAlt

--- Code/UI/UIThreading.cs
+++ Code/UI/UIThreading.cs
@@ -120,7 +120,7 @@ namespace ACME
             if (_operating)
             {
                 // Check modifier keys according to settings.
-                bool ctrlPressed = Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.LeftAlt);
+                bool ctrlPressed = Input.GetKey(KeyCode.RightAlt) || Input.GetKey(KeyCode.RightAltAlt);
                 bool shiftPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
                 // changed keybind of control to rightalt
                 // Save/restore position - is control pressed?
